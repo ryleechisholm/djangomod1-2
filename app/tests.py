773c2dbf3ad725cbd51db6387228566cc44f3fe2
ryleechisholm <89104299+ryleechisholm@users.noreply.github.com>
2021-09-17 10:43:00 -0500

    Delete app directory

--- app/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import SimpleTestCase
-
-class TestHelloView(SimpleTestCase):
-    def test_hello_view(self): 
-        response = self.client.get("/hello/nate/")
-        self.assertContains(response, "Hello nate")
-
